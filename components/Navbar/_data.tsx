b61dda6b2ff62f3e9c5997d3517045536af46e93
ian <ianbalijawa16@gmail.com>
2022-05-08 16:13:38 +0300

    changed the ui look of the navbar

---
+++ components/Navbar/_data.tsx
@@ -0,0 +1,46 @@
+import { IoCalendar, IoGrid, IoHelpBuoy } from 'react-icons/io5';
+import { MdWeb } from 'react-icons/md';
+import { ROUTES } from '../../constants/routes';
+import * as React from 'react';
+
+export interface Link {
+	label: string;
+	href?: string;
+	children?: Array<{
+		label: string;
+		description?: string;
+		href: string;
+		icon?: React.ReactElement;
+	}>;
+}
+
+export const links: Link[] = [
+	{ label: 'Systems', href: ROUTES.PRODUCTS },
+	{ label: 'PRICING', href: ROUTES.PRICING },
+	{
+		label: 'Resources',
+		children: [
+			{
+				label: 'Get Help From Documentation',
+				description:
+					'Read our documentation and FAQs, or get in touch.',
+				href: ROUTES.DOCS,
+				icon: <IoHelpBuoy />,
+			},
+			{
+				label: 'Events & Meetups',
+				description: 'Discover and join your local Sketch community.',
+				href: ROUTES.EVENTS,
+				icon: <IoCalendar />,
+			},
+			{
+				label: 'Blog',
+				description:
+					'Get updates, articles and insights from the team.',
+				href: ROUTES.BLOGS,
+				icon: <MdWeb />,
+			},
+		],
+	},
+	{ label: 'Web Design', href: ROUTES.WEBDESIGN },
+];
---
+++ components/Navbar/index.tsx
@@ -0,0 +1,28 @@
+import { Box, useColorModeValue as mode } from '@chakra-ui/react';
+import { NavContent } from './NavContent';
+
+export const NavBar = () => {
+	return (
+		<Box>
+			<Box
+				as='header'
+				bg={mode('white', 'gray.800')}
+				position='relative'
+				zIndex='10'
+			>
+				<Box
+					as='nav'
+					aria-label='Main navigation'
+					maxW='7xl'
+					mx='auto'
+					px={{ base: '6', md: '8' }}
+				>
+					<NavContent.Mobile display={{ base: 'flex', lg: 'none' }} />
+					<NavContent.Desktop
+						display={{ base: 'none', lg: 'flex' }}
+					/>
+				</Box>
+			</Box>
+		</Box>
+	);
+};
