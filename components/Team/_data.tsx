0da4988ba0aa6c57e5f01e9ea1685208bf018461
ian <ianbalijawa16@gmail.com>
2022-05-08 12:44:49 +0300

    ui:create the team component

---
+++ components/Team/_data.tsx
@@ -0,0 +1,83 @@
+export interface Member {
+	role: string;
+	image: string;
+	name: string;
+	twitter: string;
+	linkedin: string;
+	description: string;
+}
+
+export const members: Member[] = [
+	{
+		role: 'CEO',
+		image: '/images/sheena.jpg',
+		name: 'Musoke Herbet',
+		twitter: '#',
+		linkedin: '#',
+		description:
+			' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
+	},
+	{
+		role: 'Manager',
+		image: '/images/sheena2.jpg',
+		name: 'Ocen Jackson',
+		twitter: '#',
+		linkedin: '#',
+		description:
+			'Habitant morbi tristique senectus et netus et malesuada fames. Vestibulum morbi',
+	},
+	{
+		role: 'Software Programmer',
+		image: '/images/sheena.jpg',
+		name: 'Ian Balijawa',
+		twitter: '#',
+		linkedin: '#',
+		description:
+			'Quis risus sed vulputate odio ut enim blandit volutpat. Amet cursus sit amet.',
+	},
+	{
+		role: 'Secretary',
+		image: '/images/sheena.jpg',
+		name: 'Apio Juliet',
+		twitter: '#',
+		linkedin: '#',
+		description:
+			'Consectetur libero id faucibus nisl tincidunt eget nullam fringilla urna porttitor.',
+	},
+	{
+		role: 'Network Administrator',
+		image: '/images/sheena.jpg',
+		name: 'Mugabe Benedict',
+		twitter: '#',
+		linkedin: '#',
+		description:
+			'Mi eget mauris pharetra et ultrices neque ornare aenean massa eget egestas purus.',
+	},
+	{
+		role: 'Human Resource Manager',
+		image: '/images/sheena.jpg',
+		name: 'Mbazira Samson',
+		twitter: '#',
+		linkedin: '#',
+		description:
+			'Diam maecenas sed enim ut sem viverra aliquet eget magna ac placerat vestibulum.',
+	},
+	{
+		role: 'Graphics Designer',
+		image: '/images/sheena.jpg',
+		name: 'Nobert',
+		twitter: '#',
+		linkedin: '#',
+		description:
+			'Mi eget mauris pharetra et ultrices neque ornare aenean massa eget egestas purus.',
+	},
+	{
+		role: 'Head of Sales',
+		image: '/images/sheena.jpg',
+		name: 'Asha Edwin',
+		twitter: '#',
+		linkedin: '#',
+		description:
+			'Diam maecenas sed enim ut sem viverra aliquet eget magna ac placerat vestibulum.',
+	},
+];
